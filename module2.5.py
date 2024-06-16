f4a6179da356bc2987eaefe159088af975e73806
Magomeds05 <yusypovm@gmail.com>
2024-06-16 10:33:24 +0300

    Add files via upload

---
+++ module2.5.py
@@ -0,0 +1,16 @@
+
+def get_matrix(n, m, value ):  #n - строка,
+    matrix = []
+    for i in get_matrix(2, 2, 10):   #количество строк матрицы
+        matrix = []
+        for j in get_matrix(10):  # количество столбцов
+            result1 = get_matrix(2, 2, 10)
+            result2 = get_matrix(3, 5, 42)
+            result3 = get_matrix(4, 2, 13)
+
+get_matrix(2,2,10)
+
+
+
+
+
