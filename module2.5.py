0c41afc2ae36ada2ff28ddc24930a62798431414
Magomeds05 <yusypovm@gmail.com>
2024-06-17 16:38:24 +0300

    Update module2.5.py

--- module2.5.py
+++ module2.5.py
@@ -1,14 +1,14 @@
-
-def get_matrix(n, m, value ):  #n - строка,
+def get_matrix(n, m, value):  #n - строка,
     matrix = []
-    for i in get_matrix(2, 2, 10):   #количество строк матрицы
+    for i in range(n):   #количество строк матрицы
         matrix = []
-        for j in get_matrix(10):  # количество столбцов
+        for j in range(m):  # количество столбцов
             result1 = get_matrix(2, 2, 10)
             result2 = get_matrix(3, 5, 42)
             result3 = get_matrix(4, 2, 13)
 
-get_matrix(2,2,10)
+
+get_matrix()
 
 
 
